84992d974ad7afbb981e795dc9b3f4c4e7006ba0
ramda <91455804+gnfnfnr@users.noreply.github.com>
2023-03-11 21:42:38 +0900

    feat: take values of bluetooth

--- src/pages/machine/Blutooth.tsx
+++ src/pages/machine/Blutooth.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 type Props = {};
@@ -8,33 +8,77 @@ const BlutoothBox = styled.div`
 `;
 
 export default function Blutooth({}: Props) {
-  const [device, setDevice] = useState<object>({});
+  const [device, setDevice] = useState<BluetoothDevice | null>(null);
   const [support, setSupport] = useState<boolean>(true);
+  const deviceName = "HC-06";
+  const bleService = "fitness_machine";
+  const bleCharacteristic = "battery_level";
+  useEffect(() => {
+    if (!navigator.bluetooth) {
+      setSupport(false);
+    }
+  }, []);
   async function connectToDevice() {
-    await navigator.bluetooth
-      .requestDevice({
+    try {
+      const options = {
         filters: [
           {
-            name: "HC-06",
+            name: deviceName,
           },
         ],
+        optionalServices: [bleService],
         // acceptAllDevices: true,
-      })
-      .then((device) => {
-        console.log(device);
-        // device is a BluetoothDevice object
-        setDevice(device);
-      })
-      .catch((error) => {
-        console.log(error);
-        // Handle any errors
-        setSupport(error);
-      });
+      };
+      const devi = await navigator.bluetooth.requestDevice(options);
+      setDevice(devi);
+    } catch (error) {
+      console.error("Error connecting to Bluetooth device:", error);
+    }
   }
 
-  function readValue() {}
-
-  function disconnect() {}
+  async function readValue() {
+    if (device) {
+      console.log(device);
+      const infoCharacteristics = await device.gatt
+        ?.connect()
+        .then((server) => {
+          return server.getPrimaryService(bleService);
+        })
+        .then((service) => {
+          return service.getCharacteristics();
+        });
+      infoCharacteristics?.forEach(async (characteristic, index, array) => {
+        // Returns a buffer
+        console.log(characteristic);
+        const value = await characteristic.readValue();
+        console.log(value.getUint8(0));
+        // characteristic.addEventListener(
+        //   "characteristicvaluechanged",
+        //   handleBatteryLevelChanged
+        // );
+      });
+    } else {
+      console.log("연결된 블루투스가 없습니다");
+    }
+  }
+  function handleBatteryLevelChanged(event: any) {
+    if (event.target.value) {
+      let measure = event.target.value.getUint8(0);
+      console.log(measure);
+    } else {
+      console.log("rkqt ");
+    }
+  }
+  async function disconnect() {
+    try {
+      // Disconnect from the connected device
+      await device?.gatt?.disconnect();
+      // Clear the connected device from state
+      setDevice(null);
+    } catch (error) {
+      console.error("Error disconnecting from Bluetooth device:", error);
+    }
+  }
   return (
     <BlutoothBox>
       {support ? (
